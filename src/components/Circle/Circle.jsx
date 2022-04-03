dedc88ff1e58ad60963853f873f02b35c3d539ef
brayanobisto <brayan-28-@hotmail.com>
2022-04-03 00:21:57 -0500

    Change component logic

--- src/components/Circle/Circle.jsx
+++ src/components/Circle/Circle.jsx
@@ -1,7 +1,7 @@
 import styles from './Circle.module.css';
 
-function Circle({ children, checked = false }) {
-  return <div className={checked ? styles.checked : styles.circle}>{children}</div>;
+function Circle({ children }) {
+  return <div className={styles.circle}>{children}</div>;
 }
 
 export default Circle;
