68dedfe0c2ab29ff9ff856de6975baba203ab5ef
brayanobisto <brayan-28-@hotmail.com>
2022-04-03 00:23:46 -0500

    Add AttributionFooter and transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import FeedbackCard from './components/FeedbackCard/FeedbackCard';
 import ThankYouCard from './components/ThankYouCard/ThankYouCard';
+import AttributionFooter from './components/AttributionFooter/AttributionFooter';
 
 function App() {
-  const [submited, setSubmited] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
   const [valoration, setValoration] = useState('');
+  const [fadeOut, setFadeOut] = useState(false);
 
   const submitValoration = (evt) => {
     evt.preventDefault();
-    if (valoration !== '') setSubmited(true);
+    if (valoration !== '') {
+      setTimeout(() => {
+        setFadeOut(true);
+
+        setTimeout(() => {
+          setSubmitted(true);
+        }, 1000);
+      }, 1000);
+    }
   };
 
-  const handleChange = (evt) => {
+  const handleChangeValoration = (evt) => {
     setValoration(evt.target.value);
   };
 
   return (
-    <div className='app-container'>
-      {!submited ? (
-        <FeedbackCard handleChange={handleChange} valoration={valoration} submitValoration={submitValoration} />
+    <div className="app-container">
+      {!submitted ? (
+        <FeedbackCard
+          handleChangeValoration={handleChangeValoration}
+          valoration={valoration}
+          submitValoration={submitValoration}
+          fadeOut={fadeOut}
+        />
       ) : (
         <ThankYouCard valoration={valoration} />
       )}
+      <AttributionFooter />
     </div>
   );
 }
