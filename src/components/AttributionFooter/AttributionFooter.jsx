e1f2f6d1c0baad34284b90e83dba219a7685558e
brayanobisto <brayan-28-@hotmail.com>
2022-04-03 01:38:36 -0500

    Change class attribute for className

--- src/components/AttributionFooter/AttributionFooter.jsx
+++ src/components/AttributionFooter/AttributionFooter.jsx
@@ -2,7 +2,7 @@ import styles from './AttributionFooter.module.css';
 
 function AttributionFooter() {
   return (
-    <div class={styles.attribution}>
+    <div className={styles.attribution}>
       Challenge by{' '}
       <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
         Frontend Mentor
