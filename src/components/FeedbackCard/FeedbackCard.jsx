95dc6d9bc47e64748914ef11ee43ede15c804d4f
brayanobisto <brayan-28-@hotmail.com>
2022-04-03 04:59:37 -0500

    Change heading tag

--- src/components/FeedbackCard/FeedbackCard.jsx
+++ src/components/FeedbackCard/FeedbackCard.jsx
@@ -12,7 +12,7 @@ function FeedbackCard({ handleChangeValoration, valoration, submitValoration, fa
       <Circle>
         <img src={star} alt="Star" />
       </Circle>
-      <h2>How did we do?</h2>
+      <h1>How did we do?</h1>
       <p>
         Please let us know how we did with your support request. All feedback is appreciated
         to help us improve our offering!
