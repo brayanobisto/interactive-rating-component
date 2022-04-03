a38fb21dbbd80b476b245bf1a0f8b5942a5ff6a9
brayanobisto <brayan-28-@hotmail.com>
2022-04-03 00:34:58 -0500

    Add finished component

--- src/components/ThankYouCard/ThankYouCard.jsx
+++ src/components/ThankYouCard/ThankYouCard.jsx
@@ -1,8 +1,31 @@
-function ThankyouCard() {
+import { useState, useEffect } from 'react';
+import img from '../../images/illustration-thank-you.svg';
+import styles from './ThankYouCard.module.css';
+
+function ThankyouCard({ valoration }) {
+  const [fadeIn, setfadeIn] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setfadeIn(true);
+    }, 500);
+  }, []);
+
+  const className = fadeIn
+    ? `card ${styles['thankyou-card']} ${styles['fade-in']}`
+    : `card ${styles['thankyou-card']}`;
+
   return (
-    <div>
-      You selected {/* <!-- Add rating here --> */} out of 5 Thank you! We appreciate you taking the time to give a rating. If you ever
-      need more support, don’t hesitate to get in touch!
+    <div className={className}>
+      <div>
+        <img src={img} alt="Thank you ilustration" />
+        <span>You selected {valoration} out of 5</span>
+        <h1>Thank you!</h1>
+        <p>
+          We appreciate you taking the time to give a rating. If you ever need more support,
+          don’t hesitate to get in touch!
+        </p>
+      </div>
     </div>
   );
 }
